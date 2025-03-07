93a78b98bcbbb210277a2092ac704513e173ea4c
victor-prm <victorpabb@gmail.com>
2025-03-08 00:47:05 +0100

    minor change to previous push

--- js/details.js
+++ js/details.js
@@ -8,8 +8,6 @@ let bodyElm = document.body;
 
 const det_url = 'https://api.themoviedb.org/3/movie/1064213?append_to_response=videos&language=en-US';
 
-//const genre_url = `https://api.themoviedb.org/3/discover/movie?api_key=af1b76109560756a2450b61eff16e738&with_genres=28&page=2`
-
 let genre = 28;
 let page = 2;
 const genre_url = `https://api.themoviedb.org/3/discover/movie?with_genres=${genre}&page=${page}`
